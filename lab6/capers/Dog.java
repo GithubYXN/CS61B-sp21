9ee6d66597a50dfeca7def73cffe038365cbc758
GithubYXN <2502869657@qq.com>
2024-10-27 21:51:06 +0800

    finished lab6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -2,16 +2,17 @@ package capers;
 
 import java.io.File;
 import java.io.Serializable;
+
+import static capers.CapersRepository.CAPERS_FOLDER;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author yangx
 */
-public class Dog { // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = join(CAPERS_FOLDER, "dogs");
 
     /** Age of dog. */
     private int age;
@@ -39,8 +40,8 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File dogFile = join(DOG_FOLDER, name);
+        return readObject(dogFile, Dog.class);
     }
 
     /**
@@ -56,7 +57,12 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dogFile = join(DOG_FOLDER, name);
+        if (dogFile.exists()) {
+            System.out.println("Dog already exists!");
+            return;
+        }
+        writeObject(dogFile, this);
     }
 
     @Override
